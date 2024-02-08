0d3297bca69865e2e61c1e7a4a535ae62909a6c8
Justin Gardner <justin@rhynorater.com>
2024-02-08 06:03:10 -0500

    17.6.4

--- gutenberg/gutenberg.php
+++ gutenberg/gutenberg.php
@@ -5,7 +5,7 @@
  * Description: Printing since 1440. This is the development plugin for the block editor, site editor, and other future WordPress core functionality.
  * Requires at least: 6.3
  * Requires PHP: 7.0
- * Version: 17.6.3
+ * Version: 17.6.4
  * Author: Gutenberg Team
  * Text Domain: gutenberg
  *
@@ -13,8 +13,8 @@
  */
 
 ### BEGIN AUTO-GENERATED DEFINES
-define( 'GUTENBERG_VERSION', '17.6.3' );
-define( 'GUTENBERG_GIT_COMMIT', '805e36e678d42445a2fb984cf2016c045fd11ca9' );
+define( 'GUTENBERG_VERSION', '17.6.4' );
+define( 'GUTENBERG_GIT_COMMIT', '8597624dcb5402e3c951b42b7bd62fbce3e93d6e' );
 ### END AUTO-GENERATED DEFINES
 
 gutenberg_pre_init();
